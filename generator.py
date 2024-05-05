0b50e70f198ae47e76f9ec2819db4f7b576a1d44
LeonMMcoset <leonmmcoset@qq.com>
2024-05-05 12:21:54 +0800

    Normal update

--- generator.py
+++ generator.py
@@ -2,7 +2,6 @@ import os
 os.system(f'pyinstaller --add-data "venv/bin/_internal/audio:audio"\
           --add-data "bin/_internal/font:font"\
           --add-data "bin/_internal/image:image"\
-          --add-data "bin/_internal/docx:docx"\
           --add-data "bin/_internal/app:app"\
           --add-data "C:\\Users\\leonm\PycharmProjects\leonsystem\\venv\Lib\site-packages\\ursina:usina/"\
           --add-data "C:\\Users\\leonm\PycharmProjects\leonsystem\\venv\Lib\site-packages\\ursina-7.0.0.dist-info:ursina-7.0.0.dist-info"\
