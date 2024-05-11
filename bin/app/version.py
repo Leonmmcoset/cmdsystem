a8d4ab18a8ce7a34bcd2e007b928a597ab3bff1c
LeonMMcoset <leonmmcoset@qq.com>
2024-05-11 17:32:21 +0800

    Normal update

--- bin/app/version.py
+++ bin/app/version.py
@@ -20,7 +20,7 @@ def start():
     print('CMD Console')
     print('Coded by LeonMMcoset')
     print('(C)LeonMMcoset 2021-2024')
-    print('Version 25.2 Outside App Test & Fix 8(wish done!No bugs!!!)')
+    print('Version 25.2 Outside App Test & Fix 9(FUCK APP)')
     print('Windows version:',windows_version)
     print('Processor:',processor_info)
     print('RAM:',ram_total,'B')
