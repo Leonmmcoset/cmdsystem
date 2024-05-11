01eb9c482382b58430bbee26f757e1d6ce20fef6
LeonMMcoset <leonmmcoset@qq.com>
2024-05-11 15:03:03 +0800

    Normal update

--- bin/system/app.py
+++ bin/system/app.py
@@ -6,33 +6,27 @@ import importlib
 def start():
     def find_and_run_script(script_name):
         """查找并运行脚本"""
-        # 获取 _internal/outapp/py 文件夹的路径
-        script_dir = "_internal\\outapp"
+        # 获取 .internal/outapp 文件夹的路径
+        script_dir = os.path.join("_internal", "outapp")
 
-        # 遍历该文件夹中的所有文件
-        for file_name in os.listdir(script_dir):
-            # 如果文件名与输入的脚本名匹配
-            if file_name == script_name + ".py":
-                # 构建脚本文件的绝对路径
-                script_path = os.path.join(script_dir, file_name)
-                # 动态导入脚本
-                try:
-                    module = importlib.import_module(f"outapp.{script_name}")
-                    # 调用脚本中的 start() 函数
-                    module.start()
-                except Exception as e:
-                    print('Error found!It is:',e)
-                # except ModuleNotFoundError:
-                #     print(f"Script '{script_name}' not found.")
-                # except AttributeError:
-                #     print(f"Script '{script_name}' does not have a 'start()' function.")
+        # 构建脚本文件的绝对路径
+        script_path = os.path.join(script_dir, script_name + ".py")
 
+        # 检查文件是否存在
+        if os.path.exists(script_path):
+            # 动态导入脚本
+            try:
+                module = importlib.import_module(f"outapp.{script_name}", package="_internal")
+                # 调用脚本中的 start() 函数
+                module.start()
                 return True
-
-        # 如果没有找到匹配的脚本
-        print(f"No script named '{script_name}.py' found.")
-        return False
-
+            except Exception as e:
+                print('Error found! It is:', e)
+                return False
+        else:
+            # 如果没有找到匹配的脚本
+            print(f"No script named '{script_name}.py' found.")
+            return False
 
     def startapp():
         """开始函数"""
